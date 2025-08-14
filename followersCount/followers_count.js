21ec20ecb42d482eaab62c2b685c0b2b6e4428a4
Petar <vasicp92@gmail.com>
2025-08-14 09:05:59 -0400

    added resetFollower

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -15,5 +15,14 @@ function checkCountValue() {
     alert("Your Instagram post gained 10 followers! Congratulations!");
   } else if (count === 20) {
     alert("Your Instagram post gained 20 followers! Keep it up!");
+  } else if (count === 0) {
+    alert("Your Instagram followers were reseted!");
   }
+
 }
+
+function resetFollower() {
+    count=0; // reset to 1
+    displayCount(); // Display the count
+    checkCountValue(); // Check count value and display messages
+  }
