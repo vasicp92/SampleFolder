7eb4eb4a8f1818d5f2073a11b29a80bcfb92c54e
Petar <vasicp92@gmail.com>
2025-08-18 07:19:19 -0400

    Created employeDetails

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict -$22.99', 'Oatmeal -$21.99', 'Frittata -$15']
+const mainCourseMenu = ['Steak -$29', 'Pasta -$25', 'Burger -$25', 'Salmon -$29'];
+const dessertMenu = ['Cake -$10', 'Ice Cream -$10', 'Pudding -$10', 'Fruit Salad -$10'];
 
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
